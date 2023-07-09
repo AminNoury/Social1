1c2eb137437d72cfb733fb952a8f5834911362bb
Your Name <you@example.com>
2023-07-09 02:45:51 -0700

    Third commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+class UserRegisterView(View):
+    form_class = UserRegisterForm
+    template_name = 'accounts/register.html'
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password1'])
+            messages.success(request, 'you registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
+
+
+
+
+
