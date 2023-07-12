3ffa5380eb5a7670a89bf04afbea2c01b656bb0a
Your Name <you@example.com>
2023-07-12 14:48:25 -0700

    posts app added.

--- accounts/views.py
+++ accounts/views.py
@@ -56,7 +56,7 @@ class UserLogoutView(LoginRequiredMixin ,View):
         return redirect('home:home')
 
 
-class UserProfileView(LoginRequiredMixin,View):
+class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, user_id):
-        user = User.objects.get(pk=user_id)
+        user = User.objects.get(id=user_id)
         return render(request, 'accounts/profile.html', {'user': user})
---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    slug = models.SlugField()
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.slug}-{self.user}'
