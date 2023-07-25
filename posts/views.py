ec7b7505102539efc2298b6ea24965b81d008202
Your Name <you@example.com>
2023-07-25 02:52:36 -0700

    edit posts views

--- posts/views.py
+++ posts/views.py
@@ -8,11 +8,13 @@ from .forms import PostUpdateForm
 from .models import Post
 
 class PostDetailView(View):
+
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         return render(request, 'posts/detail.html', {'post': post})
 
 class PostDeleteView(LoginRequiredMixin, View):
+
     def get(self, request, post_id):
         post = Post.objects.get(pk=post_id)
         if request.user.id == post.user.id:
@@ -28,16 +30,21 @@ class PostUpdateView(LoginRequiredMixin, View):
     def setup(self, request, *args, **kwargs):
         self.post_instance = Post.objects.get(pk=kwargs['post_id'])
         super().setup(request, *args, **kwargs)
+
+
     def dispatch(self, request, *args, **kwargs):
         post = self.post_instance
         if not post.user.id == request.user.id:
             messages.error(request, 'you cant update this post', 'danger')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
+
+
     def get(self, request, *args, **kwargs):
         post = self.post_instance
         form = self.form_class(instance=post)
         return render(request, 'posts/update.html', {'form': form})
+
 
     def post(self, request, *args, **kwargs):
          post = self.post_instance
